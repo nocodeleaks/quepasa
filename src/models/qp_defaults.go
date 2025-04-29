880966ecfce078d90a8e1d252adba55aeefef1f7
No Code Leaks <codeleaks@proton.me>
2025-04-29 17:22:59 -0300

    Update qp_defaults.go

--- src/models/qp_defaults.go
+++ src/models/qp_defaults.go
@@ -7,7 +7,7 @@ import (
 
 // quepasa build version, if ends with .0 means stable versions.
 
-const QpVersion = "3.25.424.1438"
+const QpVersion = "3.25.429.1722"
 
 const QpLogLevel = log.InfoLevel
 

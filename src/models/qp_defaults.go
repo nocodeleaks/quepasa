2d7bc9006a0f6e08682edc365b9f6d281fbc9480
Arthur Ferroni <arthurferroni@gmail.com>
2025-06-09 16:21:32 -0300

    fix: update QpVersion to 3.25.609.1622

--- src/models/qp_defaults.go
+++ src/models/qp_defaults.go
@@ -7,7 +7,7 @@ import (
 
 // quepasa build version, if ends with .0 means stable versions.
 
-const QpVersion = "3.25.524.0013"
+const QpVersion = "3.25.609.1622"
 
 const QpLogLevel = log.InfoLevel
 

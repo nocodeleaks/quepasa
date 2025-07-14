0a25fc9affa2bada7b84d54373b416aa4f04f4f6
Arthur Ferroni <arthurferroni@gmail.com>
2025-07-14 12:43:55 -0300

    fix: 🐛 Update QpVersion to 3.25.1407.1244 and add latest tags for Docker images

--- src/models/qp_defaults.go
+++ src/models/qp_defaults.go
@@ -7,7 +7,7 @@ import (
 
 // quepasa build version, if ends with .0 means stable versions.
 
-const QpVersion = "3.25.1007.1350"
+const QpVersion = "3.25.1407.1244"
 
 const QpLogLevel = log.InfoLevel
 

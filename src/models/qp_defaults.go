bf6dc1afee0cf63bf9efdfce66ea4af135bf4ae8
nocodeleaks <codeleaks@proton.me>
2025-07-21 21:23:08 -0300

    fix: atualizar versão do QpVersion para 3.25.2107.2122 e adicionar novas dependências no go.sum

--- src/models/qp_defaults.go
+++ src/models/qp_defaults.go
@@ -7,7 +7,7 @@ import (
 
 // quepasa build version, if ends with .0 means stable versions.
 
-const QpVersion = "3.25.2107.1911"
+const QpVersion = "3.25.2107.2122"
 
 const QpLogLevel = log.InfoLevel
 

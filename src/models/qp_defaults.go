96f44f3e0ce7477d583fd4c822e42dfb77459070
Arthur Ferroni <93339779+arthurrferroni@users.noreply.github.com>
2025-08-20 01:55:34 -0300

    Update Quepasa version

--- src/models/qp_defaults.go
+++ src/models/qp_defaults.go
@@ -7,7 +7,7 @@ import (
 
 // quepasa build version, if ends with .0 means stable versions.
 
-const QpVersion = "3.25.2008.0121"
+const QpVersion = "3.25.2008.0155"
 
 const QpLogLevel = log.InfoLevel
 

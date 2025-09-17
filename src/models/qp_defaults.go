76019a96896d4f7f02b13a8a9b07bd2a8a169048
Arthur Ferroni <arthurferroni@gmail.com>
2025-09-17 15:53:08 -0300

    refactor: Remove undecryptable message handling and related decryption logic

--- src/models/qp_defaults.go
+++ src/models/qp_defaults.go
@@ -7,7 +7,7 @@ import (
 
 // quepasa build version, if ends with .0 means stable versions.
 // version 3.YY.MMDD.HHMM
-const QpVersion = "3.25.0916.2129"
+const QpVersion = "3.25.0917.1553"
 
 const QpLogLevel = log.InfoLevel
 

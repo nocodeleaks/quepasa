5dd198f2884b376ac5dc12bb738638c3921269f0
Deivison Lincoln <deivisonrpg@hotmail.com>
2025-10-27 18:01:21 -0300

    Removido a conexão global do rabbit antigo, estava causando duplicidade de conexão

--- src/rabbitmq/rabbitmq.go
+++ src/rabbitmq/rabbitmq.go
@@ -1,12 +1,10 @@
 package rabbitmq
 
-import (
-	environment "github.com/nocodeleaks/quepasa/environment"
-)
+//environment "github.com/nocodeleaks/quepasa/environment"
 
 // Automatically registers the RabbitMQ configuration
 func init() {
-	rabbitmq_connection_string := environment.Settings.RabbitMQ.ConnectionString
+	/*rabbitmq_connection_string := environment.Settings.RabbitMQ.ConnectionString
 	if len(rabbitmq_connection_string) > 0 {
 		rabbitmq_queue := environment.Settings.RabbitMQ.Queue
 		if len(rabbitmq_queue) > 0 {
@@ -15,5 +13,5 @@ func init() {
 
 		cachelength := environment.Settings.RabbitMQ.CacheLength
 		InitializeRabbitMQClient(rabbitmq_connection_string, cachelength)
-	}
+	}*/
 }

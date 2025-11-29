b46c4a866d88773bb9773a30f9b440f2a5a69bd9
nocodeleaks <codeleaks@proton.me>
2025-11-29 18:12:59 -0300

    feat: Return HTTP 201 on /info POST for new server creation, 200 for updates - v3.25.1129.0000

--- src/models/qp_defaults.go
+++ src/models/qp_defaults.go
@@ -9,7 +9,7 @@ import (
 
 // quepasa build version, if ends with .0 means stable versions.
 // version 3.YY.MMDD.HHMM
-const QpVersion = "3.25.1127.2142"
+const QpVersion = "3.25.1129.0000"
 
 const QpLogLevel = log.InfoLevel
 

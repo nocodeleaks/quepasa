c9f4f8cf2f7c8aab50f8c2314bf256992ba55512
nocodeleaks <codeleaks@proton.me>
2025-12-23 19:17:34 -0300

    chore: Update version to 3.25.1223.1830

--- src/models/qp_defaults.go
+++ src/models/qp_defaults.go
@@ -9,7 +9,7 @@ import (
 
 // quepasa build version, if ends with .0 means stable versions.
 // version 3.YY.MMDD.HHMM
-const QpVersion = "3.25.1223.1145"
+const QpVersion = "3.25.1223.1830"
 
 const QpLogLevel = log.InfoLevel
 

e183a0e1227c79b53bb9cfe8cdb1b086465b0b41
Arthur Ferroni <arthurferroni@gmail.com>
2025-12-24 02:31:33 -0300

    fix: Adjust foreign key checks for servers table migration 🛠️

--- src/migrations/202512231500_add_readupdate.up.sql
+++ src/migrations/202512231500_add_readupdate.up.sql
@@ -1,3 +1,7 @@
+-- Disable foreign key checks to allow dropping/recreating the servers table
+-- Required because dispatching table has FOREIGN KEY referencing servers(token)
+PRAGMA foreign_keys=off;
+
 CREATE TABLE IF NOT EXISTS `servers_202512231500` (
   `token` CHAR (100) PRIMARY KEY UNIQUE NOT NULL,
   `wid` VARCHAR (255) UNIQUE NOT NULL,
@@ -18,3 +22,6 @@ FROM `servers`;
 
 DROP TABLE `servers`;
 ALTER TABLE `servers_202512231500` RENAME TO `servers`;
+
+-- Re-enable foreign key checks
+PRAGMA foreign_keys=on;
